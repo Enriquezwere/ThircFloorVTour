1cdf422962fb81f447e59b6537d9353e6744252d
Enriquezwere <enriquez.marieleanne@gmail.com>
2023-10-19 21:55:38 -0800

    Third Floor

---
+++ data.js
@@ -0,0 +1,390 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3rd-floor",
+      "name": "3rd Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.26587854421482504,
+        "pitch": 0.06522939418374385,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3350833106837392,
+          "pitch": 0.1538511154181439,
+          "rotation": 0,
+          "target": "1-hallway-1"
+        },
+        {
+          "yaw": -3.100068431447326,
+          "pitch": 0.12279369871793122,
+          "rotation": 0,
+          "target": "6-hallway-5"
+        },
+        {
+          "yaw": 0.058147831279358186,
+          "pitch": 0.12967383367395868,
+          "rotation": 0,
+          "target": "2-hallway--2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.05422902507894989,
+          "pitch": 0.35359304515698753,
+          "title": "3rd Floor",
+          "text": "Senior High School 3rd Floor Campus."
+        }
+      ]
+    },
+    {
+      "id": "1-hallway-1",
+      "name": "Hallway 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.4470258267728706,
+        "pitch": -0.0024768109733503962,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7387141738346266,
+          "pitch": 0.25684300700800655,
+          "rotation": 0,
+          "target": "2-hallway--2"
+        },
+        {
+          "yaw": 2.760389059857495,
+          "pitch": 0.1652021722310888,
+          "rotation": 0,
+          "target": "0-3rd-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hallway--2",
+      "name": "Hallway  2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.06767603222019147,
+        "pitch": 0.01487215054998714,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.023429547368273518,
+          "pitch": 0.1436240798250985,
+          "rotation": 0,
+          "target": "3-hallway-3"
+        },
+        {
+          "yaw": 2.3886636467788236,
+          "pitch": 0.2463710326007913,
+          "rotation": 0,
+          "target": "1-hallway-1"
+        },
+        {
+          "yaw": 1.5269710889476897,
+          "pitch": 0.10187434566335263,
+          "rotation": 0,
+          "target": "0-3rd-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hallway-3",
+      "name": "Hallway 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.061847474421329096,
+        "pitch": 0.07049716734383082,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03567970382613339,
+          "pitch": 0.10741114473628066,
+          "rotation": 0,
+          "target": "4-grade-12-faculty"
+        },
+        {
+          "yaw": -3.122582729408716,
+          "pitch": 0.11238449832766939,
+          "rotation": 0,
+          "target": "2-hallway--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-grade-12-faculty",
+      "name": "Grade 12 Faculty",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.00840115842163236,
+        "pitch": -0.022967456475624815,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1020227618905203,
+          "pitch": 0.23675780711597305,
+          "rotation": 0,
+          "target": "5-hallway-4"
+        },
+        {
+          "yaw": -1.4942904523494906,
+          "pitch": 0.1237124466012105,
+          "rotation": 0,
+          "target": "3-hallway-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5747929997829822,
+          "pitch": -0.01858638944398905,
+          "title": "Faculty Room&nbsp;",
+          "text": "Teacher faculty room is a designated workspace within an educational institution where teachers and faculty members can collaborate, prepare for lessons, grade assignments, and engage in professional discussions and activities.<div><br></div>"
+        },
+        {
+          "yaw": 0.056089501873540115,
+          "pitch": -0.2127426750480801,
+          "title": "Grade 12 Advisers",
+          "text": "Grade 12 advisers are educators who assist and guide senior high school students in their academic and post-graduation planning.<br><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "5-hallway-4",
+      "name": "Hallway 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -3.0039030946154845,
+        "pitch": 0.1745928507274357,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9748198710188314,
+          "pitch": 0.16953257033883595,
+          "rotation": 0,
+          "target": "4-grade-12-faculty"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-hallway-5",
+      "name": "Hallway 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.41358863741593943,
+          "pitch": 0.01348568448300469,
+          "rotation": 0,
+          "target": "7-hallway-6"
+        },
+        {
+          "yaw": -3.021747828909998,
+          "pitch": 0.18949953934141206,
+          "rotation": 0,
+          "target": "0-3rd-floor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6429758171441513,
+          "pitch": 0.014704205178748708,
+          "title": "Ongoing Maintenance",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "7-hallway-6",
+      "name": "Hallway 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.26192085090267625,
+          "pitch": 0.024569152701102936,
+          "rotation": 0,
+          "target": "6-hallway-5"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "3rd floor Senior High",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
